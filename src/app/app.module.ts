e1f2c4e5965558fe72ad0dd055ff8ebbbe9d3828
Brey Zhou <bzu@mm-software.com>
2024-07-02 13:34:39 +0800

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    NzBreadCrumbModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    NzSelectModule,
+    NzDatePickerModule,
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient(),
+  ],
+})
+export class AppModule {}
